55edc8247471c36d6a4909feb5be7d9eba0fe73d
Carlton Lobo <carlton.lobo@outlook.com>
2024-12-02 16:20:17 +0530

    Domino Piling

---
+++ Domino Piling/Test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int height, width;
+    cout << "Enter the Width and height";
+    cin >> height >> width;
+    int output = height * width / 2;
+    cout << output;
+    return 0;
+}
